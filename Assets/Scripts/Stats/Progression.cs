20f72852793312799270451ec882e536cced4003
velcronator <valacos7@gmail.com>
2023-06-22 20:48:53 +1000

    Editing Sciptable Object Files: Yaml

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -11,9 +11,9 @@ namespace RPG.Stats
         {
             foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                if(progressionClass.characterClass == characterClass)
+                if (progressionClass.characterClass == characterClass)
                 {
-                    return progressionClass.health[level -1];
+                    //return progressionClass.health[level -1];
                 }
             }
             return 0f;
@@ -23,8 +23,14 @@ namespace RPG.Stats
         class ProgressionCharacterClass
         {
             public CharacterClass characterClass;
-            public float[] health;
+            public ProgressionStat[] stats;
+        }
 
+        [System.Serializable]
+        class ProgressionStat
+        {
+            public Stat stat;
+            public float[] levels;
         }
     }
 }
